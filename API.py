61d6380940d7c68f4bf42642dac7fec592aadaf8
Daniel <danielgonzalez3@csus.edu>
2020-10-23 03:00:54 -0700

    made into while(1)

--- API.py
+++ API.py
@@ -1,25 +1,28 @@
 #InnovationChallenge Project
 import requests
 import json
+import time
 
-with open('input.txt', 'r') as file:
-    data = file.read().split('/', 13)
-#List of the Symptoms is always - data[12]
-print(data)
-if(len(data)>2):
-    Symptoms = ((data[12].replace(" ,", ",")).replace(", ", ",")).replace(' ', '_')
-    URL = "http://localhost:5000/?symptoms="+Symptoms
-response = requests.get(URL)
-print(URL)
+while(1):
+    with open('input.txt', 'r') as file:
+        data = file.read().split('/', 13)
+        #List of the Symptoms is always - data[12]
+        print(data)
+    if(len(data)>2):
+        Symptoms = ((data[12].replace(" ,", ",")).replace(", ", ",")).replace(' ', '_')
+        URL = "http://localhost:5000/?symptoms="+Symptoms
+        response = requests.get(URL)
+    print(URL)
 
-if response.status_code == 200:
-    Output = json.loads(response.text)
-    print(Output.get('data'))
-    outFile = open("Output.txt", "w")
-    write = outFile.write(Output.get('data'))
-    outFile.close()
-elif response.status_code == 500:
-    print("Unsuffice Data Provided")
-else:
-    print("Error. Check Server")
-
+    if response.status_code == 200:
+        Output = json.loads(response.text)
+        print(Output.get('data'))
+        outFile = open("Output.txt", "w")
+        write = outFile.write(Output.get('data'))
+        outFile.close()
+    elif response.status_code == 500:
+        print("Unsuffice Data Provided")
+    else:
+        print("Error. Check Server")
+	
+    time.sleep(3)
