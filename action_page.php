06ee2e956dae4f0d0365c093c2be70f2d9ac1335
Daniel <danielgonzalez3@csus.edu>
2020-10-23 00:00:31 -0700

    PHP done.

--- action_page.php
+++ action_page.php
@@ -1,6 +1,5 @@
 <?php 
-	$newLine = "\n";
-    $Input = $_POST["firstName"].'/'.$_POST["lastName"].'/'.$_POST["lastName"].'/'.$_POST["pAge"].'/'.$_POST["birthday"].'/'.$_POST["gender"].'/'.$_POST["ethnicity1"].'/'.$_POST["ethnicity2"].'/'.$_POST["ethnicity3"].'/'.$_POST["ethnicity4"].'/'.$_POST["ethnicity5"].'/'.$_POST["ethnicity6"].'/'.$_POST["history"].'/'.$_POST["listsymptoms"].'/'.$_POST["addnote"];
-	echo $Input;
-
+    $Input = $_POST["firstName"].'/'.$_POST["lastName"].'/'.$_POST["pAge"].'/'.$_POST["birthday"].'/'.$_POST["gender"].'/'.$_POST["ethnicity1"].'/'.$_POST["ethnicity2"].'/'.$_POST["ethnicity3"].'/'.$_POST["ethnicity4"].'/'.$_POST["ethnicity5"].'/'.$_POST["ethnicity6"].'/'.$_POST["history"].'/'.$_POST["listsymptoms"].'/'.$_POST["addnote"];
+        file_put_contents("input.txt", $Input);
+	header('Location: ResultsOfSearch.html');
 ?>
