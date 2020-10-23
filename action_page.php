eca6957bee4443d5fd8d1fd594f1de01ff3fa245
Gonzalez <daniel.gonzalez@intel.com>
2020-10-22 23:18:45 -0700

    testing to mac

--- action_page.php
+++ action_page.php
@@ -1,5 +1,32 @@
 <?php 
-    $test = "this is showing";
-    $name = $_POST["firstName"];	
-    echo $name;
+    $var1 = "this is showing";
+    $var2 = $_POST["firstName"];
+	$var3 = $_POST["lastName"];	
+    $var4 = $_POST["pAge"];
+	$var5 = $_POST["birthday"];
+	$var6 = $_POST["gender"];
+	$var7 = $_POST["ethnicity1"];
+	$var8 = $_POST["ethnicity2"];
+	$var9 = $_POST["ethnicity3"];
+	$var10 = $_POST["ethnicity4"];
+	$var11 = $_POST["ethnicity5"];
+	$var12 = $_POST["ethnicity6"];
+	$var13 = $_POST["history"];
+	$var14 = $_POST["listsymptoms"];
+	$var15 = $_POST["addnote"];
+	echo $var1;
+	echo $var2;
+	echo $var3;
+	echo $var4;
+	echo $var5;
+	echo $var6;
+	echo $var7;
+	echo $var8;
+	echo $var9;
+	echo $var10;
+	echo $var11;
+	echo $var12;
+	echo $var13;
+	echo $var14;
+	echo $var15;
 ?>
