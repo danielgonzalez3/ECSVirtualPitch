80d93ca77e1a13eeedf40495a3564625e82f0174
Gonzalez <daniel.gonzalez@intel.com>
2020-10-22 23:31:28 -0700

    testing to mac

--- action_page.php
+++ action_page.php
@@ -1,19 +1,6 @@
 <?php 
 	$newLine = "\n";
-    $Input = $_POST["firstName"].'/'.$_POST["lastName"];
-	$var3 = $_POST["lastName"];	
-    $var4 = $_POST["pAge"];
-	$var5 = $_POST["birthday"];
-	$var6 = $_POST["gender"];
-	$var7 = $_POST["ethnicity1"];
-	$var8 = $_POST["ethnicity2"];
-	$var9 = $_POST["ethnicity3"];
-	$var10 = $_POST["ethnicity4"];
-	$var11 = $_POST["ethnicity5"];
-	$var12 = $_POST["ethnicity6"];
-	$var13 = $_POST["history"];
-	$var14 = $_POST["listsymptoms"];
-	$var15 = $_POST["addnote"];
+    $Input = $_POST["firstName"].'/'.$_POST["lastName"].'/'.$_POST["lastName"].'/'.$_POST["pAge"].'/'.$_POST["birthday"].'/'.$_POST["gender"].'/'.$_POST["ethnicity1"].'/'.$_POST["ethnicity2"].'/'.$_POST["ethnicity3"].'/'.$_POST["ethnicity4"].'/'.$_POST["ethnicity5"].'/'.$_POST["ethnicity6"].'/'.$_POST["history"].'/'.$_POST["listsymptoms"].'/'.$_POST["addnote"];
 	echo $Input;
 
 ?>
